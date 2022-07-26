3ca8b1752efcf1d3c2e6d4bb417a5352ef85b8dd
Larrygf02 <raulgf_02@hotmail.com>
2022-07-26 16:42:49 -0500

    config apollo server

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs } = require('./typeDefs')
+const { resolvers } = require('./resolvers')
 
 const app = express()
+app.get('/', (req, res) => res.send('Welcome to my api'))
 
 module.exports = app
 
-function start() {
+async function start() {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app})
+
+    app.use('*', (req, res) => res.status(404).send('Not found'))
+
     app.listen(3000, () => {
         console.log('Server on port', 3000)
     })
